a0f35b552d38a1e0b10dd67c641c7e670b57c048
Bola Inc <fngafua2023@gmail.com>
2020-12-12 00:24:36 -0700

    Add sign in button and set constraints for the each elements

---
+++ BankingApp/Controllers/SignInController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BankingApp
+//
+//  Created by Francis Ngafua on 10/25/20.
+//
+
+import UIKit
+
+class SignInController: UIViewController {
+
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var aboutButton: UIButton!
+    @IBOutlet weak var contactUsButton: UIButton!
+    @IBOutlet weak var helpButton: UIButton!
+    
+    override func viewDidLoad() {
+       super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '13.0'
+
+target 'BankingApp' do
+   use_frameworks!
+
+   # Pods for BankingApp
+   pod 'Firebase/Analytics'
+   pod 'Firebase/Auth'
+   pod 'Firebase/Firestore'
+  
+end
