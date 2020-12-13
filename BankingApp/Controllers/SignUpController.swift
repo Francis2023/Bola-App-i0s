f62046baae8ff5bf76487202666917ef1157e606
Bola Inc <fngafua2023@gmail.com>
2020-12-12 21:48:10 -0700

    Add sign up controller

---
+++ BankingApp/Controllers/SignUpController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpController.swift
+//  BankingApp
+//
+//  Created by Francis Ngafua on 12/12/20.
+//
+
+import UIKit
+import Firebase
+
+class SignUpController: UIViewController {
+    
+    
+    
+    
+    
+}
