53c23b75b9cf09a2450558c802319afdd4f76a47
Bola Inc <fngafua2023@gmail.com>
2020-12-13 00:17:59 -0700

    Add constraints to the elements

--- BankingApp/Controllers/SignUpController.swift
+++ BankingApp/Controllers/SignUpController.swift
@@ -11,6 +11,14 @@ import Firebase
 class SignUpController: UIViewController {
     
     
+    @IBOutlet weak var fullNameInput: UITextField!
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var phoneNumberInput: UITextField!
+    @IBOutlet weak var emailAddressInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var merchantInput: UISwitch!
+    @IBOutlet weak var signUpButton: UIButton!
+    
     
     
     
